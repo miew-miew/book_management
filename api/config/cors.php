fc25654ddeaf6bc46d07464ceb9baff7b07133d0
miew-miew <nrzanakoto011@gmail.com>
2024-12-11 17:05:27 +0100

    preparing for deployment

--- api/config/cors.php
+++ api/config/cors.php
@@ -16,12 +16,19 @@ return [
     */
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://book-management-theta-woad.vercel.app/login'],
+
+    'allowed_origins' => ['https://book-management-theta-woad.vercel.app'],
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
+
     'supports_credentials' => true,
 
 ];
