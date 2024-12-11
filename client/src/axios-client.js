e7fd605f3db62066fbbb58b69eb4215e56c7e6d9
miew-miew <nrzanakoto011@gmail.com>
2024-12-11 17:20:45 +0100

    preparing for deployment

--- client/src/axios-client.js
+++ client/src/axios-client.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-    baseURL: `${import.meta.env.VITE_API_BASE_URL}`
+    baseURL: `${import.meta.env.VITE_API_BASE_URL}`,
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-type': 'application/json',
+    }
 })
 
 axiosClient.interceptors.request.use((config) => {
